0252db3f98063f98e9b9f901620a586565a6b9ce
Tibor Claassen <tibor@gmah.net>
2015-07-07 17:48:10 +0200

    Create partially applied version of transform

--- test/index.js
+++ test/index.js
@@ -2,3 +2,4 @@
 
 require('./cli')
 require('./migrate')
+require('./transformation')
---
+++ test/transformation.js
@@ -0,0 +1,32 @@
+'use strict'
+var test       = require('redtape')(beforeEach)
+  , sinon      = require('sinon')
+  , proxyquire = require('proxyquire')
+  , events     = require('events')
+  , _          = require('lodash')
+
+var transformation, transform
+
+function beforeEach(done) {
+  transform = sinon.stub().returns(_.constant(null))
+  transformation = proxyquire('../lib/transformation', { './transform': transform })
+  done()
+}
+
+test('it partially applies data to transform', function (t) {
+  var data = [{ current: '123', deprecate: ['456'] }]
+    , doc  = { _id: '456' }
+  transformation(data)(doc)
+  t.ok(transform.calledWith(doc, data))
+  t.end()
+})
+
+test('returns result from transform', function (t) {
+  var data = [{ current: '123', deprecate: ['456'] }]
+    , doc  = { _id: '456' }
+  transform.returns([{ status: 'transformed' }])
+  var result = transformation(data)(doc)
+  t.deepEqual(result, [{ status: 'transformed' }])
+  t.end()
+
+})
