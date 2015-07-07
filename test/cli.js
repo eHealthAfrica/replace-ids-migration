0c1a76dbda931434b5617cdd5f9078235e008c1b
Tibor Claassen <tibor@gmah.net>
2015-07-07 15:00:45 +0200

    Pass db URL as argument to script

--- test/cli.js
+++ test/cli.js
@@ -12,7 +12,13 @@ function fakeStream () {
 }
 
 function fakeIo () {
-  return { stdin  : fakeStream() }
+  return { argv:  ['node', 'replace-ids-migration', 'http://host/db']
+         , stdin: fakeStream()
+         }
+}
+
+function fakePromise() {
+  return new Promise(function (resolve, reject) {})
 }
 
 function fakeJSONStream () {
@@ -20,17 +26,23 @@ function fakeJSONStream () {
   return { parse: sinon.stub().returns(parser) }
 }
 
-var cli, io, JSONStream
+var cli, io, JSONStream, parser, migrate
 
 function beforeEach (done) {
-  io          = fakeIo()
-  JSONStream  = fakeJSONStream()
-  cli         = proxyquire('../lib/cli', { 'JSONStream': JSONStream })
+  io         = fakeIo()
+  parser     = fakeStream()
+  JSONStream = fakeJSONStream()
+  migrate    = sinon.stub().returns(Promise.resolve('Migration done.'))
+
+  JSONStream.parse.returns(parser)
+
+  cli = proxyquire('../lib/cli', { 'JSONStream' : JSONStream
+                                 , './migrate'  : migrate
+                                 })
   done()
 }
 
 test('parses rows from stdin', function (t) {
-  var parser = fakeStream()
   JSONStream.parse.withArgs('*').returns(parser)
   cli(io).run()
   t.ok(io.stdin.pipe.calledWith(parser))
@@ -38,12 +50,43 @@ test('parses rows from stdin', function (t) {
 })
 
 test('handles parse errors', function (t) {
-  var parser = fakeStream()
-    , error  = new Error('Could not parse JSON')
-  JSONStream.parse.returns(parser)
+  var error = new Error('Could not parse JSON')
   cli(io).run().catch(function (catched) {
     t.equal(catched, error)
     t.end()
   })
   parser.emit('error', error)
+})
+
+test('aborts when no db URL is given', function (t) {
+  io.argv[2] = null
+  cli(io).run().catch(function (error) {
+    t.ok(error)
+    t.end()
+  })
+})
+
+test('starts migration when input finished parsing', function (t) {
+  cli(io).run().then(function () {
+    t.ok(migrate.calledOnce)
+    t.end()
+  })
+  parser.emit('finish')
+})
+
+test('resolves with promise from migration', function (t) {
+  migrate.returns(Promise.resolve('Yeah, done!'))
+  cli(io).run().then(function (result) {
+    t.equal(result, 'Yeah, done!')
+    t.end()
+  })
+  parser.emit('finish')
+})
+
+test('passes db url to migration', function (t) {
+  io.argv[2] = 'http://my-host/my-db'
+  cli(io).run()
+  parser.emit('finish')
+  t.ok(migrate.calledWith('http://my-host/my-db'))
+  t.end()
 })
